61907f7146f6eae7020922d797e22e7fa757b409
HoJoong Lee - Laptop <coforu@naver.com>
2016-02-04 19:06:18 +0900

    틀 Tag Extensions 적용

--- extensions/Profile/Profile.hooks.php
+++ extensions/Profile/Profile.hooks.php
@@ -5,28 +5,31 @@
  */
 class ProfileHooks{
 	public static function onParserFirstCallInit(&$parser){
-		//{{#profile: a}}
+		//?!
+		//$parser->disableCache();
+
+		//<> 태그 적용
+		$parser->setHook('sample', 'ProfileHooks::renderTag');
+		//Profile 틀 적용
 		$parser->setFunctionHook('profile', 'ProfileHooks::parserFunction');
-	
+		
 		return true;
 	}
 
-public static function parserFunction( $parser, $value){
-	$var1 = '<h2>틀 테스트 : '.$value.'</h2>';
-	$var2 = '[[Test1]]';
-	
-	$dbr = wfgetDB( DB_SLAVE );
-	$var3 = $dbr->selectFieldValues(
-	   'user',                  //테이블 이름
-	   'user_name',             //필드 이름
-	   '',                      //SQL문 LIKE 또는 CASEWHEN 등 사용
-	   __METHOD__,              //고정(?)
-	   array(),                 //ORDER BY 등 옵션 설정
-	   array()                  //LEFT JOIN 등 join 컨디션 사용
-  	);
+	public static function parserFunction( $parser, $value){
 
-  	$output = $var1.$var2.$var3[0];
+		$value = '<sample>Test </sample>';
 
-	return $output;
+		$output = $value;
+
+		//Tag Extensions 적용하고 싶다면 'noparse' => false
+		return array($output, 'noparse' => false);
+	}
+
+	public static function renderTag($input, array $args, Parser $parser, PPFrame $frame){
+
+		$output = $input.'success';
+
+		return htmlspecialchars($output);
 	}
 }
